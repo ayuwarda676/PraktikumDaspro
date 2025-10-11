e7574baf6da1879740aa3e7e29bad07c86052cbb
ayuwarda676 <ayu020wardatul@gmail.com>
2025-10-11 11:59:12 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS07.java
+++ jobsheet5/ifCetakKRS07.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ifCetakKRS07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.println("---Cetak KRS SIAKAD---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
