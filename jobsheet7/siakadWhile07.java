af290126d31ced2d41a6509fd9e1195e47e19f02
ayuwarda676 <ayu020wardatul@gmail.com>
2025-10-21 13:19:30 +0700

    Percobaan While 1

---
+++ jobsheet7/siakadWhile07.java
@@ -0,0 +1,39 @@
+package jobsheet7;
+import java.util.Scanner;
+public class siakadWhile07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nilai, jumlah, i = 0;
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        jumlah = sc.nextInt();
+
+        while (i < jumlah) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai = sc.nextInt();
+            i++;
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.print("Nilai tidak valid, masukkan nilai antara 0-100!");
+                continue;
+            }
+
+            if (nilai > 80 && nilai <= 100) {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah A");
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah B+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah B");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah C+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah C");
+            } else if (nilai > 39 && nilai <= 50) {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah D");
+            } else {
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah E");
+            }
+                
+        }
+    }
+    
+}
