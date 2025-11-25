df4a7a52109a5234955b8775bf416b3a2977d072
ayuwarda676 <ayu020wardatul@gmail.com>
2025-11-25 14:26:32 +0700

    Percobaan 1

---
+++ jobsheet11/kafe07.java
@@ -0,0 +1,19 @@
+package jobsheet11;
+
+public class kafe07 {
+    public static void Menu() {
+        System.out.println("=== Menu Kafe ===");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappucino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("7. ======================================");
+        System.out.println("8. Silahkan Pilih Menu yang Anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+    
+}
